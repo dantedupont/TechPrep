7fccffd51b133c58e9936b8273e791cff1d3094b
Dante Dupont <dmxdupont@gmail.com>
2025-08-13 15:40:39 -0400

    duplicates

--- largestNumber.ts
+++ largestNumber.ts
@@ -18,5 +18,4 @@ console.log(findLargestNumber([-1, 4, -100, -3, 0]));
 
 // Core algorithm solved in 9 minutes, another 6 minutes to fix type errors. 15 mins total
 // Had to look up how to correctly throw Error (on mdn docs, no cheating), I was rusty
-// Even though its working as I expected, I'm concerned about the number of assertions that I used
 // Time complexity: O(N)
